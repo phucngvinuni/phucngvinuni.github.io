f86ef0814f8dd2ae2141e9077b186b86575de3af
Nguyễn Phúc <144587564+phucngvinuni@users.noreply.github.com>
2025-11-23 00:23:06 +0700

    vewfwe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,27 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		lightTheme: "retro", // name of the light theme
-		darkTheme: "synthwave", // name of one of the included themes for dark mode
+		themes: [
+			{
+				linear: {
+					"primary": "#5E6AD2",
+					"secondary": "#8A8F98",
+					"accent": "#C292EB",
+					"neutral": "#1C1D21",
+					"base-100": "#08090A",
+					"base-200": "#141517",
+					"base-300": "#1C1D21",
+					"base-content": "#F7F8F8",
+					"info": "#3ABFF8",
+					"success": "#36D399",
+					"warning": "#FBBD23",
+					"error": "#F87272",
+				},
+			},
+			"light",
+			"dark",
+		],
+		darkTheme: "linear", // name of one of the included themes for dark mode
 		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
 }
